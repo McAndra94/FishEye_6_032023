f3f1ee5926e5fbd74aa3dde025ceff43b507e963
Alexandra <91855591+McAndra94@users.noreply.github.com>
2023-03-28 19:17:41 +0200

    Update media.js

---
+++ scripts/factories/media.js
@@ -0,0 +1,32 @@
+function mediaFactory(artMediaData) {
+  // Retrieve data from json file (see index.js)
+  const { id, photographerId, title, image } = artMediaData;
+
+  // Photographer cards
+  function getMediaCardDOM() {
+    // Create DOM elements & define value
+    const article = document.createElement("article");
+    const anchor = document.createElement("a");
+    // anchor.setAttribute("src", place bigger image + `${image}`);
+
+    const imgElement = document.createElement("img");
+    imgElement.setAttribute(
+      "src",
+      "assets/images/" + `${photographerId}/${image}`
+    );
+    imgElement.setAttribute("alt", `${title}`);
+    const titleElement = document.createElement("h2");
+    const likesElement = document.createElement("p");
+
+    // Attach elements to anchor with appendChild function
+    anchor.appendChild(imgElement);
+    anchor.appendChild(titleElement);
+    anchor.appendChild(likesElement);
+    // Attach anchor to article | each article directs to link
+    article.appendChild(anchor);
+
+    return article;
+  }
+
+  return { getMediaCardDOM };
+}
