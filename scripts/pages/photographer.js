204be4eff26b6112d58e3b31e0263598cf0b41a6
Alexandra <91855591+McAndra94@users.noreply.github.com>
2023-03-28 19:18:35 +0200

    Update photographer.js

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,9 +1,6 @@
-//const u = new URLSearchParams(window.location.search);
-//const photographerId = u.get("id");
-
 // Retrieve id from url
 const photographeId = new URLSearchParams(window.location.search).get("id");
-
+//
 window.onload = function () {
   fetch("/data/photographers.json")
     .then((response) => response.json())
@@ -12,19 +9,20 @@ window.onload = function () {
       const photographer = data.photographers.find(
         (item) => item.id == photographeId
       );
-      console.log(photographeId); // id (photographers)
-      // Media data
-      const photographerMedia = data.media.find(
-        (items) => items.photographerId == photographeId
-      );
-      console.log(photographerMedia); // photographer media
       if (!photographer) {
         window.location.href = "index.html";
       }
+      console.log(photographeId);
+      // Media data
+      const photographerMedia = [];
+      data.media.forEach((item) => {
+        if (item.photographerId == photographeId) photographerMedia.push(item);
+      });
+      console.log(photographerMedia.photographerId);
+
       displayPhotographer(photographer);
       console.log(photographer); // photographer info
-      if (!photographer.id === photographerMedia.photographerId) {
-      }
+
       displayMedia(photographerMedia);
     });
 };
@@ -47,23 +45,3 @@ function displayPhotographer(photographer) {
   ).src = `assets/photographers/${photographer.portrait}`;
   document.getElementById("photographerImg").alt = `${photographer.name}`;
 }
-
-// Media
-async function displayData(media) {
-  const photographerMedia = document.querySelector("#photographerMedia");
-  console.log(photographerMedia);
-}
-
-function displayMedia(photographerMedia) {
-  document.querySelector(
-    ".mediaImg"
-  ).src = `assets/images/${photographerMedia.photographerId}/${photographerMedia.image}`;
-  document.querySelector(".mediaImg").alt = `${photographerMedia.title}`;
-
-  document.getElementById("mediaTitle").textContent = photographerMedia.title;
-  document.getElementById("mediaLikes").textContent = photographerMedia.likes;
-
-  document.getElementById("likeHeart").src = "assets/images/likeHeart.png";
-  document.getElementById("likeHeart").alt = "likes";
-}
-media.forEach(photographerMedia);
