d91deb5c19e909c352cab3d6598faa5915192ff3
Alexandra <91855591+McAndra94@users.noreply.github.com>
2023-04-11 00:42:35 +0200

    Update 09/04

--- scripts/factories/media.js
+++ scripts/factories/media.js
@@ -1,27 +1,59 @@
 function mediaFactory(artMediaData) {
-  // Retrieve data from json file (see index.js)
-  const { id, photographerId, title, image } = artMediaData;
+  // Retrieve data
+  const { photographerId, title, image, video, likes } = artMediaData;
 
-  // Photographer cards
+  // Media cards
   function getMediaCardDOM() {
     // Create DOM elements & define value
     const article = document.createElement("article");
-    const anchor = document.createElement("a");
-    // anchor.setAttribute("src", place bigger image + `${image}`);
+    article.className = "mediaBox";
 
-    const imgElement = document.createElement("img");
-    imgElement.setAttribute(
-      "src",
-      "assets/images/" + `${photographerId}/${image}`
-    );
-    imgElement.setAttribute("alt", `${title}`);
+    const anchor = document.createElement("a");
+    anchor.href = "javascript:openLightbox()"; // Lightbox goes here
+    anchor.alt = title;
+
+    let mediaElement;
+    if (image) {
+      mediaElement = document.createElement("img");
+      mediaElement.src = `assets/images/${photographerId}/${image}`;
+      mediaElement.alt = title;
+      mediaElement.className = "mediaElement";
+    } else if (video) {
+      mediaElement = document.createElement("video");
+      mediaElement.src = `assets/images/${photographerId}/${video}`;
+      mediaElement.alt = title;
+      mediaElement.controls = true; // Video buttons & controls
+      mediaElement.className = "mediaElement";
+    } else {
+      return null;
+    }
+
     const titleElement = document.createElement("h2");
-    const likesElement = document.createElement("p");
+    titleElement.textContent = title;
+
+    const mediaSubBox = document.createElement("div");
+    const likeBox = document.createElement("div");
+
+    const mediaLikes = document.createElement("p");
+    mediaLikes.textContent = likes;
+    mediaLikes.className = "mediaLikes";
+    const likeHeart = document.createElement("img");
+    likeHeart.src = "assets/images/likeHeart.png";
+    likeHeart.alt = "likes";
+    likeHeart.className = "likeHeart";
 
     // Attach elements to anchor with appendChild function
-    anchor.appendChild(imgElement);
-    anchor.appendChild(titleElement);
-    anchor.appendChild(likesElement);
+    anchor.appendChild(mediaElement);
+    anchor.appendChild(mediaSubBox);
+
+    // Attach Title & Rest to upper container
+    mediaSubBox.appendChild(titleElement);
+    mediaSubBox.appendChild(likeBox);
+
+    // Attach Likes & Heart to div container
+    likeBox.appendChild(mediaLikes);
+    likeBox.appendChild(likeHeart);
+
     // Attach anchor to article | each article directs to lightbox
     article.appendChild(anchor);
 
