f8e4c0b22782352745e0aa2a88e92d80704cfde2
Alexandra <91855591+McAndra94@users.noreply.github.com>
2023-04-24 19:30:24 +0200

    Update 24/04

--- scripts/pages/media.js
+++ scripts/pages/media.js
@@ -4,13 +4,62 @@ async function displayMedia(mediaData) {
   mediaData.forEach((media) => {
     const mediaModel = mediaFactory(media);
     const userMediaCardDOM = mediaModel.getMediaCardDOM();
+    /* LightBox section */
+    console.log(media.image);
+
+    userMediaCardDOM.addEventListener("click", () => {
+      const lightBox = document.createElement("div");
+      lightBox.classList.add("lightbox");
+      console.log(media.photographerId);
+      console.log(media.image);
+      console.log(media.video);
+      console.log("media page");
+
+      // Media title in lightbox (put under image/video)
+      const lightBoxTitle = document.createElement("p");
+      lightBoxTitle.textContent = media.title;
+
+      const lightBoxMedia =
+        media.type === "image"
+          ? document.createElement("img")
+          : document.createElement("video");
+      if (media.type === "image") {
+        lightBoxMedia.src = `/assets/images/${media.photographerId}/${media.image}`;
+        console.log(media.photographerId);
+        console.log(media.image);
+      } else if (media.type === "video") {
+        lightBoxMedia.src = `/assets/images/${media.photographerId}/${media.video}`;
+        lightBoxMedia.controls = true; // Video buttons & controls
+      }
+
+      lightBoxMedia.alt = media.title;
+      lightBoxMedia.className = "lightboxMedia";
+      console.log(lightBoxMedia);
+      console.log(lightBoxMedia.src); // Error: <empty string>
+
+      const lightBoxClose = document.createElement("span");
+      lightBoxClose.classList.add("lightbox-close");
+      lightBoxClose.textContent = "x";
+      lightBoxClose.addEventListener("click", () => {
+        lightBox.remove();
+      });
+
+      lightBox.appendChild(lightBoxTitle);
+      lightBox.appendChild(lightBoxMedia);
+      lightBox.appendChild(lightBoxClose);
+      document.body.appendChild(lightBox);
+    });
+
     mediaSection.appendChild(userMediaCardDOM);
   });
 }
 
-async function displayMediaCards(mediaData) {
+function displayMediaCards(mediaData) {
   for (let i = 0; i < mediaData.length; i++) {
     const media = mediaData[i];
-    displayMediaCards(media);
+    displayMedia(media);
   }
 }
+
+const footerText = document.getElementsByClassName("footerText");
+footerText.textContent = `${price} €/jour`;
