4ae0d8c8a8e32158b0b06c28270180a3e0132ab7
Alexandra <91855591+McAndra94@users.noreply.github.com>
2023-05-15 20:04:31 +0200

    Update 15/05

--- scripts/factories/media.js
+++ scripts/factories/media.js
@@ -1,7 +1,10 @@
+let totalLikes = 0;
+
 function mediaFactory(artMediaData) {
   // Retrieve data
   const { photographerId, title, image, video, likes } = artMediaData;
 
+  console.log(artMediaData.likes, "factories");
   // Media cards
   function getMediaCardDOM() {
     // Create DOM elements & define value
@@ -9,18 +12,6 @@ function mediaFactory(artMediaData) {
     article.className = "mediaBox";
 
     const anchor = document.createElement("a");
-    // if image ? do this : do this if not
-    /* 
-    const mediaUrl = image
-      ? `assets/images/${photographerId}/${image}`
-      : `assets/images/${photographerId}/${video}`;
-    anchor.href = `javascript:openLightbox('${mediaUrl}')`; 
-    */
-    //anchor.href = "#";
-    /* anchor.addEventListener("click", (event) => {
-      event.preventDefault(); // Prevents return to top page (default function)
-      openLightbox();
-    }); */
     anchor.alt = title;
     console.log(title);
 
@@ -62,14 +53,17 @@ function mediaFactory(artMediaData) {
       if (!liked) {
         // Like +1 fonction when clicked
         artMediaData.likes++;
+        totalLikes++; // +1 on totalLikes
         mediaLikes.textContent = artMediaData.likes;
         liked = true;
       } else {
         // Like -1 fonction when clicked again
         artMediaData.likes--;
+        totalLikes--; // -1 on totalLikes
         mediaLikes.textContent = artMediaData.likes;
         liked = false;
       }
+      likesFooter.textContent = totalLikes; // updates likes in footer
     });
 
     // Attach elements to anchor with appendChild function
@@ -90,5 +84,51 @@ function mediaFactory(artMediaData) {
     return article;
   }
 
+  totalLikes += likes;
+
+  console.log(totalLikes);
+
+  // Fixed footer section
+  const likesFooter = document.querySelector(".likesFooter");
+  likesFooter.textContent = totalLikes;
+
   return { getMediaCardDOM };
+}
+
+// Sort By Popularity (likes) | method for numbers
+const sortLikesElement = document.querySelector(".sortLikes");
+sortLikesElement.addEventListener("click", function () {
+  sortByPopularity(mediaArray);
+});
+
+function sortByPopularity(popularity) {
+  const sortedMediaPopu = popularity.sort((a, b) => b.likes - a.likes);
+  mediaSection.innerHTML = "";
+  displayMedia(sortedMediaPopu);
+}
+
+// Sort By Date (New to Old)
+const sortDateElement = document.querySelector(".sortDate");
+sortDateElement.addEventListener("click", function () {
+  sortByDate(mediaArray);
+});
+
+function sortByDate(date) {
+  const sortedMediaDate = date.sort(
+    (a, b) => new Date(a.date) - new Date(b.date)
+  );
+  mediaSection.innerHTML = "";
+  displayMedia(sortedMediaDate);
+}
+
+// Sort By Title (A to Z) | method for string to sort alphabetically
+const sortTitleElement = document.querySelector(".sortTitle");
+sortTitleElement.addEventListener("click", function () {
+  sortByTitle(mediaArray);
+});
+
+function sortByTitle(title) {
+  const sortedMediaTitle = title.sort((a, b) => a.title.localeCompare(b.title));
+  mediaSection.innerHTML = "";
+  displayMedia(sortedMediaTitle);
 }
